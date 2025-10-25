75714f31a603129ff0d7584b5b71d1ab36103d67
Miguel Julio <fitfab@yahoo.com>
2025-10-25 17:33:31 -0400

    add cloudflare server setup

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "@solidjs/start/config"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  server: {
+    preset: "cloudflare_pages",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-})
+});
