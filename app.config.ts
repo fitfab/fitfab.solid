17bd25987eb3ad12bff943b68caf6cf6471ba740
Miguel Julio <fitfab@yahoo.com>
2025-10-26 12:27:00 -0400

    feat: cloudflare setup

--- app.config.ts
+++ app.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
-    preset: "cloudflare_pages",
-    rollupConfig: {
-      external: ["node:async_hooks"],
-    },
+    preset: "cloudflare_module",
+    compatibilityDate: "2025-10-24",
   },
   vite: {
     plugins: [tailwindcss()],
